6dfc92de6efcf59540090aeeacb8ea8aa536b7af
Benur21 <29681989+Benur21@users.noreply.github.com>
2022-05-22 19:47:00 +0100

    fixed components no resizing on window resize

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+function render() {
+  ReactDOM.render(<App />, document.getElementById('root'));
+}
+
+render();
+
+window.addEventListener('resize', render);
